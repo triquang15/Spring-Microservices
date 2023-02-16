b3669421c9136fc1532bb7b431d2a6f8517671c6
qnguyen95 <quangtri.nguyen@dxc.com>
2023-02-16 17:05:29 +0700

    Using Model Map

--- rest-api/src/main/java/com/devteam/service/impl/UserServiceImpl.java
+++ rest-api/src/main/java/com/devteam/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +18,19 @@ import com.devteam.service.UserService;
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 
 	@Override
 	public UserDto createUser(UserDto userDto) {
-		User user = UserMapper.mapToUser(userDto);
+		//User user = UserMapper.mapToUser(userDto);		
+		User user = modelMapper.map(userDto, User.class);
 
 		User savedUser = userRepository.save(user);
 
-		UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+	//	UserDto savedUserDto = UserMapper.mapToUserDto(savedUser);
+		UserDto savedUserDto = modelMapper.map(savedUser, UserDto.class);
 
 		return savedUserDto;
 	}
@@ -34,13 +40,17 @@ public class UserServiceImpl implements UserService {
 		Optional<User> optionalUser = userRepository.findById(userId);
 
 		User user = optionalUser.get();
-		return UserMapper.mapToUserDto(user);
+		
+	//	return UserMapper.mapToUserDto(user);
+		return modelMapper.map(user, UserDto.class);
 	}
 
 	@Override
 	public List<UserDto> getAllUsers() {
 		List<User> users = userRepository.findAll();
-		return users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+		
+	//	return users.stream().map(UserMapper::mapToUserDto).collect(Collectors.toList());
+		return users.stream().map((user) -> modelMapper.map(user, UserDto.class)).collect(Collectors.toList());
 	}
 
 	@Override
@@ -51,7 +61,9 @@ public class UserServiceImpl implements UserService {
 		existingUser.setEmail(user.getEmail());
 
 		User updatedUser = userRepository.save(existingUser);
-		return UserMapper.mapToUserDto(updatedUser);
+		
+//		return UserMapper.mapToUserDto(updatedUser);
+		return modelMapper.map(updatedUser, UserDto.class);
 	}
 
 	@Override
