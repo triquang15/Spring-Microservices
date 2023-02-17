8dae97847511414331dc67337d19c83b19a55c6e
qnguyen95 <quangtri.nguyen@dxc.com>
2023-02-17 18:34:29 +0700

    Service Registry and Discoverry using Cloud Nextflit Eureka

--- Spring-Cloud/employee-service/src/main/java/com/devteam/EmployeeServiceApplication.java
+++ Spring-Cloud/employee-service/src/main/java/com/devteam/EmployeeServiceApplication.java
@@ -2,10 +2,12 @@ package com.devteam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
